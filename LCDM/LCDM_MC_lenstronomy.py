17327849d512f0431f6f3c0e24abb9c74cb79d69
CHENGYI <chengyi_wang@outlook.com>
2022-04-23 10:04:42 +0800

    Add the example of LCDM

---
+++ LCDM/LCDM_MC_lenstronomy.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 11:16:05 2022
+
+@author: CHENG
+"""
+
+import lens_model_class
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.optimize as opt
+import h5py
+import emcee
+import corner
+import lens_model_class
+from lens_model_class import lens_redshift_difference
+from lenstronomy.LensModel.lens_model import LensModel
+from lenstronomy.LensModel.Solver.lens_equation_solver import LensEquationSolver
+
+global number
+number = 0
+
+def lnprior(p):
+    # The parameters are stored as a vector of values, so unpack them
+    # If the parameters are out of expected range, set the value -inf
+    # If not return 0 and calculate likehood
+    Omega_M, Omega_Lambda  = p
+    # We're using only uniform priors, and only eps has a lower bound
+    if Omega_M < 0.0  or Omega_M > 1.0 :
+        return - np.inf
+    elif Omega_Lambda < 0.0 or Omega_Lambda > 1.0:
+        return - np.inf
+    #elif Omega_M+Omega_Lambda < 0.0 or Omega_M+Omega_Lambda > 1.0:
+    #   return - np.inf
+    #elif H0 < 50 or H0 > 90:
+    #    return - np.inf
+
+       
+    return 0
+
+def lnlike(p, zl, zs, Deltaz, z_err):
+    #likehood function, use mean square function here
+    
+    
+    global number
+    
+    
+    Omega_M, Omega_Lambda = p
+    
+    redshift_differences = lens.lens_redshift_difference([0, 0], kwargs_lens, cosmology_model,\
+            zl, zs, [63, Omega_M, Omega_Lambda],search_window=80,min_distance=30)
+    
+    # the likelihood is sum of the lot of normal distributions
+    
+    denom = np.power(z_err,2)
+    lp = - 0.5 * sum( np.power((Deltaz - redshift_differences),2)/denom + np.log(denom))
+    
+    #number = number + 1
+    #if number%100 == 0:
+    #    print("i:likehood",number,",",p,",lp=",lp)
+    
+    return lp
+
+
+
+def lnprob(p, zl_true, zs_true, Delta_z_obs, z_err):
+    
+    global number
+    
+    lp = lnprior(p) # test whether the vector is not out range
+    
+    if not np.isfinite(lp):
+        
+        #number = number + 1 
+        #if number% 100 == 0:
+        #    print("i:prior",number,",",p,",lp=-inf")    
+        return - np.inf
+    
+    return lp + lnlike(p, zl_true, zs_true, Delta_z_obs, z_err) # If not compute likehood
+
+
+
+
+zl_true,zs_true,Delta_z_true,Delta_z_obs = np.load("LCDM10-8.npy")
+z_err = 1e-8
+Omega_M_true = 0.3
+Omega_Lambda_true = 0.7
+H0_true = 63
+
+#nll = lambda *args: -lnlike(*args)
+#result = opt.minimize(nll, [Omega_M_true, Omega_Lambda_true, H0_true, eps_true],
+#                      args=(zl_true,zs_true,Delta_z_obs))
+
+nwalkers, ndim = 10, 2
+
+# create a nearby proir 
+p0 = np.array([[0.3, 0.7 ]])
+p0 = np.array([ p0*( 1 + 1.e-9*np.random.randn(ndim))\
+               for i in range(nwalkers)]).reshape(nwalkers,ndim)
+
+
+filename = 'LCDM.h5'
+backend = emcee.backends.HDFBackend(filename)
+backend.reset(nwalkers, ndim)
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob,\
+                                args=[zl_true, zs_true, Delta_z_obs, z_err],\
+                                    backend=backend)
+    
+#track how the average autocorrelation time estimate changes
+steps = 10000
+index = 0
+autocorr = np.empty(steps)
+
+# This will be useful to testing convergence
+old_tau = np.inf
+
+#choice of lens mdoel
+lens_nie = 'NIE'
+lens_model_list = [lens_nie]
+
+#def parameter values of lens models
+kwargs_nie = {'theta_E':40, 'e1':0.5, 'e2':-0.5,\
+              's_scale':0.1, 'center_x':0.1, 'center_y':0.1}
+kwargs_lens = [kwargs_nie]
+cosmology_model = 'LambdaCDM'
+
+lens = lens_redshift_difference(lens_model_list)
+
+
+#sample for up to max_n steps
+for sample in sampler.sample(p0, iterations=steps, progress=True, tune=True):
+    # Only check convergence every 100 steps
+    if sampler.iteration % 100:
+        continue
+    
+    # Compute the autocorrelation time so far
+    # Using tol=0 means
+    #always get an estimate even if it isn't trustworthy
+    tau = sampler.get_autocorr_time(tol=0)
+    autocorr[index] = np.mean(tau)
+    #print(tau)
+    index = index + 1
+    
+    # Check convergence
+    converged = np.all(tau * 100 < sampler.iteration)
+    converged &= np.all(np.abs(old_tau - tau) / tau < 0.01)
+    if converged:
+        break
+    old_tau = tau
+    
+    
+    
+
+'''
+pos,prob,state = sampler.run_mcmc(p0 , 5000, progress=True,\
+                                  tune=True, store=True)
+
+result = sampler.flatchain[np.where(np.sum(sampler.flatchain,axis=1)<1)\
+                           or np.where(sampler.flatchain.any>1)\
+                               or np.where(sampler.flatchain.any<0)]
+tmp = corner.corner(result[2000:],\
+            labels=['$\Omega_M$', '$\Omega_\Lambda$'],\
+                truths=[Omega_M_true, Omega_Lambda_true],\
+                    bins=40,hist_bin_factor=5,\
+                        quantiles=(0.16, 0.84))
+
+pygtc.plotGTC(chains=[result[2000:]],chainLabels=["LCDM"]\
+              ,paramNames=["$\Omega_M$","$\Omega_\Lambda$"],\
+                  truths=(0.3,0.7),truthLabels='Standard',\
+                      nContourLevels=3,figureSize='APJ_page')
+'''
+
+
+'''
+tmp = pygtc.plotGTC(result,sigmaContourLevels=True,\
+                    truths=[0.3,0.7],\
+                        paramNames=['$\Omega_M$','$\Omega_\Lambda$'],\
+                            figureSize='APJ_page',nBins=50,nContourLevels=3)
+'''
+
+
+
+'''
+plt.plot(autocorr[autocorr>0])
+plt.xlabel('steps')
+plt.ylabel('autocorrelation time $\tau$')
+'''
+
+
---
+++ LCDM/LCDM_fake_lenstronomy.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 23:43:24 2022
+
+@author: CHENG
+"""
+
+import lens_model_class
+from lens_model_class import lens_redshift_difference
+import numpy as np
+from lenstronomy.LensModel.lens_model import LensModel
+from lenstronomy.LensModel.Solver.lens_equation_solver import LensEquationSolver
+
+eps_true = 0.2
+Nobs = 10
+beta = [0,0]
+err = 1e-8
+
+#choice of lens mdoel
+lens_nie = 'NIE'
+lens_spep = 'SPEP'
+lens_shear = 'SHEAR'
+lens_sis = 'SIS'
+lens_model_list = [lens_nie]
+
+#set up model
+lensModel = LensModel(lens_model_list=lens_model_list)
+
+
+#def parameter values of lens models
+kwargs_nie = {'theta_E':40, 'e1':0.5, 'e2':-0.5,\
+              's_scale':0.1, 'center_x':0.1, 'center_y':0.1}
+kwargs_spep = {'theta_E': .9, 'e1': 0.05, 'e2': 0.05, 
+               'gamma': 2., 'center_x': 0.1, 'center_y': 0}
+kwargs_shear = {'gamma1':0.5, 'gamma2':0.1, 'ra_0':0.0, 'dec_0':0.0}
+kwargs_sis = {'theta_E':40,'center_x':0.5,'center_y':0.1}
+kwargs_lens = [kwargs_nie]
+
+zl_true = np.random.uniform(1, 4, size=Nobs)
+zs_true = zl_true + np.random.uniform(2.5 , 3.5, size=Nobs)
+Omega_M_true = 0.3
+Omega_Lambda_true = 0.7
+H0_true = 72
+
+Delta_z_true = []
+Delta_z_obs = []
+lens = lens_redshift_difference(lens_model_list)
+cosmology_model = 'LambdaCDM'
+
+for i in range(Nobs):
+    temp = lens.lens_redshifts(beta, kwargs_lens, cosmology_model,\
+            zl_true[i], zs_true[i], [H0_true, Omega_M_true, Omega_Lambda_true])
+    temp = temp.max() - temp.min()
+    temp2 = temp  + err * np.random.normal(0, eps_true)
+    
+    Delta_z_true.append(temp)
+    Delta_z_obs.append(temp2)
+    
+    print(i,",",temp,",",temp2)
+
+#Delta_z_obs = Delta_z_true * ( 1 + ( np.random.normal(0,eps_true,size=Nobs) ) )
+#np.random.random(Nobs)
+
+np.save("LCDM10-8.npy",[zl_true,zs_true,Delta_z_true,Delta_z_obs])
+
+
+
+
+
+
+
